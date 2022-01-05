d062d212e74e161a3db0fb6a535a0040deedfed5
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-01-04 17:53:58 -0700

    Creating server, creating server as class, create routes, configure app.js to call server class, create middlewares for public files, create middleawares for cors

---
+++ models/server.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const cors = require('cors')
+
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT
+        
+        //Middlewares
+        this.middlewares();
+
+        //Application Routes
+        this.routes();
+    }
+
+    middlewares(){
+
+        //CORS
+        this.app.use( cors() );
+
+        //Public Folder
+        this.app.use( express.static('public') );
+        
+
+    }
+
+    routes(){
+        this.app.get('/api', (req, res)=>{
+            res.json({
+                "msg": "get API"
+            })
+        })
+
+        this.app.put('/api', (req, res)=>{
+            res.status(400).json({
+                "msg": "put API"
+            })
+        })
+
+        this.app.post('/api', (req, res)=>{
+            res.status(201).json({
+                "msg": "post API"
+            })
+        })
+
+        this.app.delete('/api', (req, res)=>{
+            res.json({
+                "msg": "delete API"
+            })
+        })
+
+        this.app.patch('/api', (req, res)=>{
+            res.json({
+                "msg": "patch API"
+            })
+        })
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log("Servidor corriendo en puerto", this.port)
+        })
+    }
+
+}
+
+
+module.exports = Server
