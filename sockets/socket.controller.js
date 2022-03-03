be0313ff1f99ab1c7995ebec7c986439f397da18
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-03-03 13:34:49 -0700

    finish complete chat with private message without design complete, autentication, subscribe to channel and sending private messages.

--- sockets/socket.controller.js
+++ sockets/socket.controller.js
@@ -14,8 +14,13 @@ const socketController = async( socket = new Socket, io )=>{
         return socket.disconnect();
     }
 
+    //Add user connected
     chatMessages.userConnected(user)
-    io.emit('active-users', chatMessages.usersArr);
+    io.emit('active-users',         chatMessages.usersArr);
+    socket.emit('recieve-messages', chatMessages.lastTen);
+
+    //Como el socket id cambia cada que el usuario se conecta hay que conectarlo a una sala especial
+    socket.join( user.id ) // va a tener tres salas el usuario, la global, por el socket.id y por el user.id
 
     //clean when someone is disconnected
     socket.on('disconnect', ()=>{
@@ -24,9 +29,14 @@ const socketController = async( socket = new Socket, io )=>{
     })
 
     socket.on('send-message', ({ uid, message })=>{
-        console.log("data", uid + ' ' + message)
-        chatMessages.sendMessage( user.id,  user.name, message);
-        io.emit('recieve-messages', chatMessages.lastTen); 
+        if(uid){
+            //si existe el uid es un mensaje privado, en caso contrario es para todo el mundo
+            socket.to(uid).emit('private-message', { from: user.name, message });
+        }else{
+            console.log("data", uid + ' ' + message)
+            chatMessages.sendMessage( user.id,  user.name, message);
+            io.emit('recieve-messages', chatMessages.lastTen); 
+        }
     })
 
 }
